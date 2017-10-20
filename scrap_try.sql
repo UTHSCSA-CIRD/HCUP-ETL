bf8b5fab333e50ec75cfd2b15aa432982c5f1ab9
Daniel MacCarthy <MacCarthyD@uthscsa.edu>
2017-10-20 16:55:58 -0500

    createed procs and diags

--- scrap_try.sql
+++ scrap_try.sql
@@ -2,6 +2,97 @@
 /*
 hcup project scrap file 
 */
+--drop table procedures ;
+
+create table procedures as
+select key, pr1 pr,1 prime_proc from hcup2008 where pr1 is not null and pr1 !='' union
+select key, pr2 pr,0 prime_proc from hcup2008 where pr2 is not null and pr2 !='' union
+select key, pr3 pr,0 prime_proc from hcup2008 where pr3 is not null and pr3 !='' union
+select key, pr4 pr,0 prime_proc from hcup2008 where pr4 is not null and pr4 !='' union
+select key, pr5 pr,0 prime_proc from hcup2008 where pr5 is not null and pr5 !='' union
+select key, pr6 pr,0 prime_proc from hcup2008 where pr6 is not null and pr6 !='' union
+select key, pr7 pr,0 prime_proc from hcup2008 where pr7 is not null and pr7 !='' union
+select key, pr8 pr,0 prime_proc from hcup2008 where pr8 is not null and pr8 !='' union
+select key, pr9 pr,0 prime_proc from hcup2008 where pr9 is not null and pr9 !='' union
+select key, pr10 pr,0 prime_proc from hcup2008 where pr10 is not null and pr10 !='' union
+select key, pr11 pr,0 prime_proc from hcup2008 where pr11 is not null and pr11 !='' union
+select key, pr12 pr,0 prime_proc from hcup2008 where pr12 is not null and pr12 !='' union
+select key, pr13 pr,0 prime_proc from hcup2008 where pr13 is not null and pr13 !='' union
+select key, pr14 pr,0 prime_proc from hcup2008 where pr14 is not null and pr14 !='' union
+select key, pr15 pr,0 prime_proc from hcup2008 where pr15 is not null and pr15 !='' union
+select key, pr1 pr,1 prime_proc from hcup2009 where pr1 is not null and pr1 !='' union
+select key, pr2 pr,0 prime_proc from hcup2009 where pr2 is not null and pr2 !='' union
+select key, pr3 pr,0 prime_proc from hcup2009 where pr3 is not null and pr3 !='' union
+select key, pr4 pr,0 prime_proc from hcup2009 where pr4 is not null and pr4 !='' union
+select key, pr5 pr,0 prime_proc from hcup2009 where pr5 is not null and pr5 !='' union
+select key, pr6 pr,0 prime_proc from hcup2009 where pr6 is not null and pr6 !='' union
+select key, pr7 pr,0 prime_proc from hcup2009 where pr7 is not null and pr7 !='' union
+select key, pr8 pr,0 prime_proc from hcup2009 where pr8 is not null and pr8 !='' union
+select key, pr9 pr,0 prime_proc from hcup2009 where pr9 is not null and pr9 !='' union
+select key, pr10 pr,0 prime_proc from hcup2009 where pr10 is not null and pr10 !='' union
+select key, pr11 pr,0 prime_proc from hcup2009 where pr11 is not null and pr11 !='' union
+select key, pr12 pr,0 prime_proc from hcup2009 where pr12 is not null and pr12 !='' union
+select key, pr13 pr,0 prime_proc from hcup2009 where pr13 is not null and pr13 !='' union
+select key, pr14 pr,0 prime_proc from hcup2009 where pr14 is not null and pr14 !='' union
+select key, pr15 pr,0 prime_proc from hcup2009 where pr15 is not null and pr15 !='' 
+
+;
+-- drop table diagnoses ;
+create table diagnoses as 
+select key, dx1 dx,1 prime_dx from hcup2008 where dx1 is not null and dx1 !='' union
+select key, dx2 dx,0 prime_dx from hcup2008 where dx2 is not null and dx2 !='' union
+select key, dx3 dx,0 prime_dx from hcup2008 where dx3 is not null and dx3 !='' union
+select key, dx4 dx,0 prime_dx from hcup2008 where dx4 is not null and dx4 !='' union
+select key, dx5 dx,0 prime_dx from hcup2008 where dx5 is not null and dx5 !='' union
+select key, dx6 dx,0 prime_dx from hcup2008 where dx6 is not null and dx6 !='' union
+select key, dx7 dx,0 prime_dx from hcup2008 where dx7 is not null and dx7 !='' union
+select key, dx8 dx,0 prime_dx from hcup2008 where dx8 is not null and dx8 !='' union
+select key, dx9 dx,0 prime_dx from hcup2008 where dx9 is not null and dx9 !='' union
+select key, dx10 dx,0 prime_dx from hcup2008 where dx10 is not null and dx10 !='' union
+select key, dx11 dx,0 prime_dx from hcup2008 where dx11 is not null and dx11 !='' union
+select key, dx12 dx,0 prime_dx from hcup2008 where dx12 is not null and dx12 !='' union
+select key, dx13 dx,0 prime_dx from hcup2008 where dx13 is not null and dx13 !='' union
+select key, dx14 dx,0 prime_dx from hcup2008 where dx14 is not null and dx14 !='' union
+select key, dx15 dx,0 prime_dx from hcup2008 where dx15 is not null and dx15 !='' union
+select key, dx1 dx,1 prime_dx from hcup2009 where dx1 is not null and dx1 !='' union
+select key, dx2 dx,0 prime_dx from hcup2009 where dx2 is not null and dx2 !='' union
+select key, dx3 dx,0 prime_dx from hcup2009 where dx3 is not null and dx3 !='' union
+select key, dx4 dx,0 prime_dx from hcup2009 where dx4 is not null and dx4 !='' union
+select key, dx5 dx,0 prime_dx from hcup2009 where dx5 is not null and dx5 !='' union
+select key, dx6 dx,0 prime_dx from hcup2009 where dx6 is not null and dx6 !='' union
+select key, dx7 dx,0 prime_dx from hcup2009 where dx7 is not null and dx7 !='' union
+select key, dx8 dx,0 prime_dx from hcup2009 where dx8 is not null and dx8 !='' union
+select key, dx9 dx,0 prime_dx from hcup2009 where dx9 is not null and dx9 !='' union
+select key, dx10 dx,0 prime_dx from hcup2009 where dx10 is not null and dx10 !='' union
+select key, dx11 dx,0 prime_dx from hcup2009 where dx11 is not null and dx11 !='' union
+select key, dx12 dx,0 prime_dx from hcup2009 where dx12 is not null and dx12 !='' union
+select key, dx13 dx,0 prime_dx from hcup2009 where dx13 is not null and dx13 !='' union
+select key, dx14 dx,0 prime_dx from hcup2009 where dx14 is not null and dx14 !='' union
+select key, dx15 dx,0 prime_dx from hcup2009 where dx15 is not null and dx15 !='' union
+select key, dx16 dx,0 prime_dx from hcup2009 where dx16 is not null and dx16 !='' union
+select key, dx17 dx,0 prime_dx from hcup2009 where dx17 is not null and dx17 !='' union
+select key, dx18 dx,0 prime_dx from hcup2009 where dx18 is not null and dx18 !='' union
+select key, dx19 dx,0 prime_dx from hcup2009 where dx19 is not null and dx19 !='' union
+select key, dx20 dx,0 prime_dx from hcup2009 where dx20 is not null and dx20 !='' union
+select key, dx21 dx,0 prime_dx from hcup2009 where dx21 is not null and dx21 !='' union
+select key, dx22 dx,0 prime_dx from hcup2009 where dx22 is not null and dx22 !='' union
+select key, dx23 dx,0 prime_dx from hcup2009 where dx23 is not null and dx23 !='' union
+select key, dx24 dx,0 prime_dx from hcup2009 where dx24 is not null and dx24 !='' union
+select key, dx25 dx,0 prime_dx from hcup2009 where dx25 is not null and dx25 !='' 
+;
+
+
+
+select * from hcup2008 where pr1 != '';
+; 
+
+-- drop table diags
+create table diags as 
+
+; 
+
+
+create table visit as 
 SELECT
     AGE,
     AGEDAY,
@@ -23,36 +114,6 @@ SELECT
     DRG_NoPOA,
     DSHOSPID,
 /*
-    DX1,
-    DX2,
-    DX3,
-    DX4,
-    DX5,
-    DX6,
-    DX7,
-    DX8,
-    DX9,
-    DX10,
-    DX11,
-    DX12,
-    DX13,
-    DX14,
-    DX15,
-    DXCCS1,
-    DXCCS2,
-    DXCCS3,
-    DXCCS4,
-    DXCCS5,
-    DXCCS6,
-    DXCCS7,
-    DXCCS8,
-    DXCCS9,
-    DXCCS10,
-    DXCCS11,
-    DXCCS12,
-    DXCCS13,
-    DXCCS14,
-    DXCCS15,
     ECODE1,
     ECODE2,
     ECODE3,
